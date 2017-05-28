e0ecec07204ec97ee7ad096d2a8a729afdffb875
MichaelHazani <michael@michaelhazani.com>
2017-05-27 23:44:54 -0700

    add procfile, ignore

---
+++ server/ignore/creds.js
@@ -0,0 +1,11 @@
+//fractalicio.us creds
+var creds = {
+    consumer_key: 'syqWCbOuhulkLIEdwvY9FSMNb',
+    consumer_secret: 'UD24vuwIXDHYBJUgoSrE0ds67yl0dN24WDDwpSFtujECT4WHEF',
+    access_token_key: '866509790431723520-1quR8utuLVMzKJ7DXE9w3eSV9cSRt59',
+    access_token_secret: 'Wo8WkmcrBLEK56vwSVkXYMBROSqwIc2A9xBZKnGcCGVLr',
+    slLayer: '2885acca6ad7f2f0fdf9cae1d3478d25'
+}
+
+
+module.exports = creds;
