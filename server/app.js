3ce6e296633ed30e208a6ef17b5f5d92cc466e09
MichaelHazani <michael@michaelhazani.com>
2017-05-28 00:01:41 -0700

    solve heroku express port issue

--- server/app.js
+++ server/app.js
@@ -99,6 +99,6 @@ setInterval(function () {
 //     counter++;
 // })
 
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
     console.log('Example app listening on port 3000!');
 });
